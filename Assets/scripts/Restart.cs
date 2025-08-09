bc962e76cd896b98c54221d9b1ed97cae3c2aa33
harusato1122 <11haru.s22@gmail.com>
2025-08-09 23:22:23 +0900

    リスタート追加

---
+++ Assets/scripts/Restart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
